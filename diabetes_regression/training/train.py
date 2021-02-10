f54ce0439c2e4168b3894b7cd4f7ed42b8c1026c
Rohit-Suryawanshi <59337066+Rohit-Suryawanshi@users.noreply.github.com>
2021-02-10 19:03:50 +0530

    Update train.py

--- diabetes_regression/training/train.py
+++ diabetes_regression/training/train.py
@@ -45,7 +45,7 @@ def split_data(df):
 
 # Train the model, return the model
 def train_model(data, ridge_args):
-    reg_model = Lasso(**ridge_args)
+    reg_model = Ridge(**ridge_args)
     reg_model.fit(data["train"]["X"], data["train"]["y"])
     return reg_model
 
